f5d9213f1b067c8685efd297b0a7a12a455edf0a
Alex Bezerra <alexgbezerra@outlook.com>
2020-04-10 16:10:18 -0300

    Projeto Criado

---
+++ src/application/Program.java
@@ -0,0 +1,31 @@
+package application;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Digite seu nome : ");
+		String nome = sc.nextLine();
+		
+		
+		System.out.print("Sua idade:");
+		sc.nextLine();
+		
+		System.out.print("Local de Nascimento:");
+		sc.nextLine();
+		
+		System.out.println();
+		
+		System.out.println("Obrigado Deus pelo dom da vida ");
+		
+		
+		
+		
+		sc.close();
+	}
+
+}
